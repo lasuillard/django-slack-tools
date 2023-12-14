efd5a46db514cc48c2f5bf649016d1b253247c3e
Yuchan Lee <lasuillard@gmail.com>
2023-12-14 13:59:46 +0000

    refactor: Move custom field to utils

--- django_slack_bot/models/message_recipient.py
+++ django_slack_bot/models/message_recipient.py
@@ -1,43 +1,10 @@
 """Message recipients model."""
 from __future__ import annotations
 
-from typing import Any
-
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-
-class SeparatedValuesField(models.TextField):
-    """Custom field for storing list of string items."""
-
-    def __init__(self, *args: Any, separator: str | None = None, **kwargs: Any) -> None:  # noqa: D107
-        self.separator = separator or ","
-        super().__init__(*args, **kwargs)
-
-    def deconstruct(self) -> Any:  # noqa: D102
-        name, path, args, kwargs = super().deconstruct()
-        if self.separator != ",":
-            kwargs["separator"] = self.separator
-
-        return name, path, args, kwargs
-
-    def from_db_value(self, value: Any, *args: Any, **kwargs: Any) -> Any:  # noqa: ARG002, D102
-        return self.to_python(value)
-
-    def to_python(self, value: str | list | None) -> list[str] | None:  # noqa: D102
-        if not value:
-            return None
-
-        if isinstance(value, list):
-            return value
-
-        return value.split(self.separator)
-
-    def get_db_prep_value(self, value: list[str] | None, *args: Any, **kwargs: Any) -> str | None:  # noqa: D102, ARG002
-        if not value:
-            return None
-
-        return self.separator.join(value)
+from django_slack_bot.utils.fields import SeparatedValuesField
 
 
 class SlackMessageRecipient(models.Model):
