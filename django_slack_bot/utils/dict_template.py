e933ef77a6117106c6cd16d8fbc3f141c4ebe43c
Yuchan Lee <lasuillard@gmail.com>
2023-12-24 11:42:38 +0000

    feat: Simple dict template

---
+++ django_slack_bot/utils/dict_template.py
@@ -0,0 +1,39 @@
+"""Utils for dictionary-based templates."""
+from __future__ import annotations
+
+from typing import Any, TypeVar
+
+
+def render(template: dict, **kwargs: Any) -> dict:
+    """Render given dictionary template.
+
+    Args:
+        template: Dictionary template.
+        kwargs: Keyword arguments passed to template.
+
+    Returns:
+        Rendered dictionary.
+    """
+    # Forget the original to format it in-place
+    result = template.copy()
+    for k, v in result.items():
+        result[k] = _format_obj(v, **kwargs)
+
+    return result
+
+
+T = TypeVar("T", dict, list, str)
+
+
+def _format_obj(obj: T, **kwargs: Any) -> T:
+    """Format object recursively."""
+    if isinstance(obj, dict):
+        return {k: _format_obj(v, **kwargs) for k, v in obj.items()}
+
+    if isinstance(obj, str):
+        return obj.format_map(kwargs)
+
+    if isinstance(obj, list):
+        return [_format_obj(item, **kwargs) for item in obj]
+
+    return obj
