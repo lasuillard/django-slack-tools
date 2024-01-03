bd8e95a8279e459fa1e97daaf77c96f0ddab6341
Yuchan Lee <lasuillard@gmail.com>
2024-01-03 13:34:12 +0000

    feat: Display mention name

--- django_slack_bot/admin/mention.py
+++ django_slack_bot/admin/mention.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.contrib.admin.filters import ChoicesFieldListFilter, DateFieldListFilter
 from django.utils.translation import gettext_lazy as _
 
+from django_slack_bot.app_settings import app_settings
+from django_slack_bot.choices import MentionType
 from django_slack_bot.models import SlackMention
 
 
@@ -10,7 +12,25 @@ from django_slack_bot.models import SlackMention
 class SlackMentionAdmin(admin.ModelAdmin):
     """Admin for recipients."""
 
-    readonly_fields = ("id", "created", "last_modified")
+    readonly_fields = ("id", "_get_mention_name", "created", "last_modified")
+
+    @admin.display(description=_("Mention Name"))
+    def _get_mention_name(self, instance: SlackMention) -> str:
+        workspace_info = app_settings.backend.get_workspace_info()
+
+        if instance.type == MentionType.USER:
+            for member in workspace_info["members"]:
+                if "<@{id}>".format(id=member["id"]) == instance.mention:
+                    return str(member["profile"]["display_name"])
+            return "?"
+
+        if instance.type == MentionType.GROUP:
+            for usergroup in workspace_info["usergroups"]:
+                if "<!subteam^{id}>".format(id=usergroup["id"]) == instance.mention:
+                    return str(usergroup["name"])
+            return "?"
+
+        return ""
 
     # Actions
     actions = ()
@@ -19,7 +39,7 @@ class SlackMentionAdmin(admin.ModelAdmin):
     # ------------------------------------------------------------------------
     date_hierarchy = "last_modified"
     search_fields = ("name", "mention")
-    list_display = ("id", "name", "type", "mention", "created", "last_modified")
+    list_display = ("id", "name", "type", "mention", "_get_mention_name", "created", "last_modified")
     list_display_links = ("id", "name", "mention")
     list_filter = (
         ("type", ChoicesFieldListFilter),
@@ -33,7 +53,7 @@ class SlackMentionAdmin(admin.ModelAdmin):
         (
             None,
             {
-                "fields": ("name", "type", "mention"),
+                "fields": ("name", "type", "mention", "_get_mention_name"),
             },
         ),
         (
