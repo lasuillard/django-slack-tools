4f41c4e8c3603bac423eb4bcd6596c664996af67
Yuchan Lee <lasuillard@gmail.com>
2024-09-29 21:19:59 +0900

    chore(devcontainer): Remove redundant package
    Signed-off-by: Yuchan Lee <lasuillard@gmail.com>

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -5,5 +5,4 @@ curl -fsSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/tr
     && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
 
 sudo apt-get update && sudo apt-get install -y \
-    bash-completion \
     ngrok
