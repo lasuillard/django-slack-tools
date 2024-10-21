3d52418f80d33e6f70b8327ee0985061348c7de8
Yuchan Lee <lasuillard@gmail.com>
2024-10-21 12:17:01 +0000

    chore: Update periodic task example

--- examples/todo/todo/celery.py
+++ examples/todo/todo/celery.py
@@ -3,6 +3,7 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
 
 from celery import Celery
+from celery.schedules import crontab
 from django.conf import settings
 
 app = Celery("todo")
@@ -14,18 +15,13 @@ if settings.USE_TZ:
 
 app.conf.beat_scheduler = "django_celery_beat.schedulers:DatabaseScheduler"
 app.conf.beat_schedule = {
-    "add-every-30-seconds": {
-        "task": "tasks.add",
-        "schedule": 30.0,
-        "args": (16, 16),
+    "Cleanup old Slack messages": {
+        "task": "django_slack_tools.slack_messages.tasks.cleanup_old_messages",
+        "schedule": crontab(minute=0, hour=4),  # Every 4 AM
+        "kwargs": {
+            "threshold_seconds": 1 * 24 * 60 * 60,  # Retain for a day
+        },
     },
 }
-
-
-# TODO(lasuillard): Just for testing, remove this.
-@app.task(name="tasks.add")
-def add(x: float, y: float) -> float:  # noqa: D103
-    return x + y
-
 
 app.autodiscover_tasks()
