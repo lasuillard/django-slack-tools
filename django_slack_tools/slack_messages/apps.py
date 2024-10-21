5bc7837d266addad867e60810d0bd483daf92e1f
Yuchan Lee <lasuillard@gmail.com>
2024-10-21 12:50:13 +0000

    chore: Ignore some code from coverage

--- django_slack_tools/slack_messages/apps.py
+++ django_slack_tools/slack_messages/apps.py
@@ -10,7 +10,7 @@ class DjangoSlackBotConfig(AppConfig):  # noqa: D101
     name = "django_slack_tools.slack_messages"
     verbose_name = _("Slack Messages")
 
-    def ready(self) -> None:
+    def ready(self) -> None:  # pragma: no cover
         """Auto-discover Celery tasks, if Celery is installed."""
         try:
             import celery  # noqa: F401
