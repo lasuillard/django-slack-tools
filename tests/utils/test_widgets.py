6a86939e02ef63f93771b20bfedf89b3c54bbdfd
Yuchan Lee <lasuillard@gmail.com>
2025-01-20 12:30:40 +0000

    Revert "ci: Inject failure for test"
    This reverts commit 4dd24ac7498913855f0f60a46a5c5c7e5768073b.

--- tests/utils/test_widgets.py
+++ tests/utils/test_widgets.py
@@ -3,9 +3,6 @@ from django_slack_tools.utils.widgets import JSONWidget
 
 class TestJSONWidget:
     def test_format_value(self) -> None:
-        # TODO(lasuillard): Fault injection test for parametrized extras; should be removed before PR merged
-        import celery  # noqa: F401
-
         widget = JSONWidget()
 
         # Pretty format with some indents
