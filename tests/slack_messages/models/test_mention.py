15a13bb6bfb8efdd71b521148a83feb081ca197d
Yuchan Lee <lasuillard@gmail.com>
2025-01-21 14:10:28 +0000

    chore: Make linter happy

--- tests/slack_messages/models/test_mention.py
+++ tests/slack_messages/models/test_mention.py
@@ -15,20 +15,20 @@ class TestSlackMention(ModelTestBase):
     factory_cls = SlackMentionFactory
 
     @pytest.mark.parametrize(
-        argnames=["kwargs", "expect"],
+        argnames=("kwargs", "expect"),
         argvalues=[
-            [
+            (
                 {"type": SlackMention.MentionType.SPECIAL, "name": "Here", "mention_id": "<!here>"},
                 "Here (Special, <!here>)",
-            ],
-            [
+            ),
+            (
                 {"type": SlackMention.MentionType.USER, "name": "lasuillard", "mention_id": "U0000000000"},
                 "lasuillard (User, U0000000000)",
-            ],
-            [
+            ),
+            (
                 {"type": SlackMention.MentionType.GROUP, "name": "Backend", "mention_id": "T0000000000"},
                 "Backend (Group, T0000000000)",
-            ],
+            ),
         ],
         ids=["special", "user", "team"],
     )
@@ -37,20 +37,20 @@ class TestSlackMention(ModelTestBase):
         assert str(instance) == expect
 
     @pytest.mark.parametrize(
-        argnames=["kwargs", "expect"],
+        argnames=("kwargs", "expect"),
         argvalues=[
-            [
+            (
                 {"type": SlackMention.MentionType.SPECIAL, "name": "Here", "mention_id": "<!here>"},
                 "<!here>",
-            ],
-            [
+            ),
+            (
                 {"type": SlackMention.MentionType.USER, "name": "lasuillard", "mention_id": "U0000000000"},
                 "<@U0000000000>",
-            ],
-            [
+            ),
+            (
                 {"type": SlackMention.MentionType.GROUP, "name": "Backend", "mention_id": "T0000000000"},
                 "<!subteam^T0000000000>",
-            ],
+            ),
         ],
         ids=["special", "user", "team"],
     )
