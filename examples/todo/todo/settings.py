d53c122a75c646817107fe802741dd74e021f5e2
Yuchan Lee <lasuillard@gmail.com>
2025-02-10 13:09:43 +0000

    fix: Update example app configuration

--- examples/todo/todo/settings.py
+++ examples/todo/todo/settings.py
@@ -136,15 +136,35 @@ LOGGING = {
     },
 }
 
-SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN", default="i-am-a-cookie")
-SLACK_SIGNING_SECRET = os.environ.get("SLACK_SIGNING_SECRET", default="stupid-potato")
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
 
 DJANGO_SLACK_TOOLS = {
-    "SLACK_APP": "todo.slack_app.app",
-    "BACKEND": {
-        "NAME": "django_slack_tools.slack_messages.backends.SlackBackend",
-        "OPTIONS": {
-            "slack_app": "todo.slack_app.app",
+    "slack_app": "todo.slack_app.app",
+    "messengers": {
+        "default": {
+            "class": "django_slack_tools.slack_messages.messenger.Messenger",
+            "kwargs": {
+                "template_loaders": [
+                    "django_slack_tools.slack_messages.template_loaders.DjangoTemplateLoader",
+                    "django_slack_tools.slack_messages.template_loaders.DjangoPolicyTemplateLoader",
+                ],
+                "middlewares": [
+                    {
+                        "class": "django_slack_tools.slack_messages.middlewares.DjangoDatabasePolicyHandler",
+                        "kwargs": {
+                            "messenger": "default",
+                        },
+                    },
+                    "django_slack_tools.slack_messages.middlewares.DjangoDatabasePersister",
+                ],
+                "messaging_backend": {
+                    "class": "django_slack_tools.slack_messages.backends.SlackBackend",
+                    "kwargs": {
+                        "slack_app": "todo.slack_app.app",
+                    },
+                },
+            },
         },
     },
 }
